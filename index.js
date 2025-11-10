be7e4a4aa7efb0aa225dd6800d47e403033a943a
unknown <tomalhossen78@gmail.com>
2025-11-10 18:20:39 +0600

    my-contributions api added

--- index.js
+++ index.js
@@ -25,16 +25,38 @@ async function run() {
 
     const cleanlinessDB = client.db('cleanlinessDB');
     const issuesCollections = cleanlinessDB.collection('issuesCollections');
+    const myContributionsCollections = cleanlinessDB.collection('myContributionsCollections')
 
 
     app.post('/issues',async(req,res)=>{
         const data = req.body;
-        const result = await issuesCollections.insertOne(data)
-        res.send(result)
+        const result = await issuesCollections.insertOne(data);
+        res.send(result);
+    })
+
+    app.post('/my-contributions',async(req,res)=>{
+      const data = req.body;
+      const result = await myContributionsCollections.insertOne(data);
+      res.send(result);
+    })
+    app.get('/my-contributions',async(req,res)=>{
+      const email = req.query.email;
+      const result = await myContributionsCollections.find({email : email}).toArray()
+      res.send(result);
     })
     app.get('/issues',async(req,res)=>{
-        const result = await issuesCollections.find().toArray();
+        const result = await issuesCollections.find().sort({date : -1}).toArray();
         res.send(result);
+    })
+    app.get('/issues/:id',async(req,res)=>{
+      const id = req.params.id;
+        const result = await issuesCollections.findOne({_id : new ObjectId(id)});
+        res.send(result);
+    })
+
+    app.get('/recent-complaints',async(req,res)=>{
+      const result = await issuesCollections.find().sort({date : -1}).limit(4).toArray();
+      res.send(result);
     })
 
     app.get(`/issues`,async(req,res)=>{
